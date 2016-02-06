ff9e71f37af1fc0c2fac14e214c55ba26e00187b
Casey Carroll <aiyakc@gmail.com>
2016-02-06 09:09:44 -0600

    added bind and listen

--- server.c
+++ server.c
@@ -6,13 +6,36 @@
 int main()
 {
 
-	int sock;
+	int bindSuccess, sock, isListening;
+	int queueSize = 5;
+	struct my_addr;
+	unsigned short listen_port=6000;
 
+	//create socket
 	sock=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
+	//check if socket creation successful
 	if(sock < 0) 
 	{
 		printf("socket() failed\n");
+	}
+
+	//make local address structure
+	memset(&my_addr, 0, sizeof(my_addr)); //zero out structure
+	my_addr.sin_family = AF_INET; //set address family
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY); //set IP
+	my_addr.sin_port = htons(listen_port);
+
+	bindSuccess = bind(sock, (struct sockaddr *) &my_addr, sizeof (my_addr));
+	if(bindSuccess < 0)
+	{
+		printf("bind() failed\n");
+	}
+
+	isListening=listen(sock, queueSize);
+	if (isListening < 0) 
+	{
+		printf("listen() failed\n");
 	}
 
 	return 0;
