317d118d4ba4fce4dd9a2a2be089f5097bc63e92
Casey Carroll <aiyakc@gmail.com>
2016-02-05 18:40:33 -0600

    first few lines of code and gitignore file

--- server.c
+++ server.c
@@ -1,3 +1,19 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+
+int main()
+{
+
+	int sock;
+
+	sock=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if(sock < 0) 
+	{
+		printf("socket() failed\n");
+	}
+
+	return 0;
+}
